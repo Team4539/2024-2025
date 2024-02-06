9310045ba9dac864f91b0e2579321c1cb4ff55d8
KAOTIC Robotics <98129325+KAOTIC-Robotics-4539@users.noreply.github.com>
2024-02-06 13:07:21 -0600

    Added for arm max

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,7 +22,8 @@ public final class Constants {
     {
         public static final int armID = 17;
         public static final int armInvertedID = 18;
-        public static final double armMin = -0.5;
+        public static final double armMin = 0.1;
+        public static final double armMax = 45;
     }
     public static final class Climb {
         public static final int climbID = 19 ;

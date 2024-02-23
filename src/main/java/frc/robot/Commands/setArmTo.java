947147b4b94423380c939c600bbef83a89248892
Agame7k <comaidenaiden@gmail.com>
2024-02-23 13:22:11 -0600

    latest

--- src/main/java/frc/robot/Commands/setArmTo.java
+++ src/main/java/frc/robot/Commands/setArmTo.java
@@ -19,7 +19,7 @@ public class setArmTo extends Command
         m_arm = subsystem;
         m_target = targetrot;
         m_command = command;
-        pidController = new PIDController(0.15, 0.002, 0.0); // Adjust these values as needed
+        pidController = new PIDController(0.175, 0.002, 0.0); // Adjust these values as needed
 
     }
 

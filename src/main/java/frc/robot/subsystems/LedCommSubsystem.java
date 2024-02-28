0e96a25bad6f723547c1f91ae81c38a76419ffdf
Agame7k <comaidenaiden@gmail.com>
2024-02-27 20:32:13 -0600

    Add DigitalInput for receiving data from Arduino

--- src/main/java/frc/robot/subsystems/LedCommSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedCommSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -7,20 +8,21 @@ import frc.robot.Constants;
 
 public class LedCommSubsystem extends SubsystemBase{
     private PWM pwmSnd;
-    private PWM pwmRcv;
+    private DigitalInput pwmRcv;
 
     public LedCommSubsystem()
     {
         pwmSnd = new PWM(Constants.arduinoCOMs.arduinoSend);
-        pwmRcv = new PWM(Constants.arduinoCOMs.arduinoRcv);
+        pwmRcv = new DigitalInput(Constants.arduinoCOMs.arduinoRcv);
         
     }
     public void periodic(){
-        if (pwmRcv.getSpeed() >= 1){
+        if (pwmRcv.get() == false){
             SmartDashboard.putBoolean("NOTE", true);
         }else {
             SmartDashboard.putBoolean("NOTE", false);
         }
+        SmartDashboard.putBoolean("DATA", pwmRcv.get());
     }   
     public void setLed(double speed)
     {

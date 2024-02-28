daaa1189bc7561bd9d51255577607502e67f3080
Agame7k <comaidenaiden@gmail.com>
2024-02-28 08:07:46 -0600

    E

--- src/main/java/frc/robot/subsystems/LedCommSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedCommSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -8,21 +9,21 @@ import frc.robot.Constants;
 
 public class LedCommSubsystem extends SubsystemBase{
     private PWM pwmSnd;
-    private DigitalInput pwmRcv;
+    private DutyCycleEncoder pwmRcv;
 
     public LedCommSubsystem()
     {
         pwmSnd = new PWM(Constants.arduinoCOMs.arduinoSend);
-        pwmRcv = new DigitalInput(Constants.arduinoCOMs.arduinoRcv);
+        pwmRcv = new DutyCycleEncoder(Constants.arduinoCOMs.arduinoRcv);
         
     }
     public void periodic(){
-        if (pwmRcv.get() == false){
+        /*if (pwmRcv.getDistance()){
             SmartDashboard.putBoolean("NOTE", true);
         }else {
             SmartDashboard.putBoolean("NOTE", false);
-        }
-        SmartDashboard.putBoolean("DATA", pwmRcv.get());
+        }*/
+        SmartDashboard.putNumber("Data", pwmRcv.getDistance());
     }   
     public void setLed(double speed)
     {

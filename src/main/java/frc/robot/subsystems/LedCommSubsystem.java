a151f53ac3adda662a14534534e2a9366db9e9b2
Agame7k <comaidenaiden@gmail.com>
2024-02-27 19:31:34 -0600

    Update Constants and SwerveRequest values

--- src/main/java/frc/robot/subsystems/LedCommSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedCommSubsystem.java
@@ -1,19 +1,29 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class LedCommSubsystem extends SubsystemBase{
-    private PWM pwm;
+    private PWM pwmSnd;
+    private PWM pwmRcv;
 
     public LedCommSubsystem()
     {
-        pwm = new PWM(Constants.arduinoCOMs.arduinoCOM);
+        pwmSnd = new PWM(Constants.arduinoCOMs.arduinoSend);
+        pwmRcv = new PWM(Constants.arduinoCOMs.arduinoRcv);
+        
     }
-    
+    public void periodic(){
+        if (pwmRcv.getSpeed() >= 1){
+            SmartDashboard.putBoolean("NOTE", true);
+        }else {
+            SmartDashboard.putBoolean("NOTE", false);
+        }
+    }   
     public void setLed(double speed)
     {
-        pwm.setSpeed(speed);
+        pwmSnd.setSpeed(speed);
     }
 }

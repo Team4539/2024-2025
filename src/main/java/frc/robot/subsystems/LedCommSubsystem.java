9d99a1f3265935c4508f775a6590436774362c7b
Agame7k <comaidenaiden@gmail.com>
2024-02-28 13:54:09 -0600

    Update SmartDashboard.putNumber() method in LedCommSubsystem.java

--- src/main/java/frc/robot/subsystems/LedCommSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedCommSubsystem.java
@@ -24,7 +24,7 @@ public class LedCommSubsystem extends SubsystemBase{
         }else {
             SmartDashboard.putBoolean("NOTE", false);
         }*/
-        SmartDashboard.putNumber("Data", pwmRcv.getAverageValue());
+        SmartDashboard.putNumber("Data", pwmRcv.getValue());
     }   
     public void setLed(double speed)
     {

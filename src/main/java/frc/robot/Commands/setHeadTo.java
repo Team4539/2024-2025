95e97377c8c45c0e9334898efb3df0d2e84ea1ac
Agame7k <comaidenaiden@gmail.com>
2024-03-19 12:09:09 -0500

    l

--- src/main/java/frc/robot/Commands/setHeadTo.java
+++ src/main/java/frc/robot/Commands/setHeadTo.java
@@ -19,7 +19,7 @@ public class setHeadTo extends Command
         m_arm = subsystem;
         m_target = targetrot;
         m_command = command;
-        pidController = new PIDController(0.175, 0.002, 0.0); // TODO: Adjust these values as needed
+        pidController = new PIDController(0.175, 0.002, 0.0); //    
 
     }
 

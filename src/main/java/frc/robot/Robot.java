26ee28eac0255a367c359fcda4b6c768e791c74e
sstock2005 <sstock2005@outlook.com>
2024-04-09 19:27:17 -0500

    limelight

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,6 +27,11 @@ public class Robot extends TimedRobot
     m_robotContainer = new RobotContainer();
     // m_led = m_robotContainer.m_led;
     PortForwarder.add(5800, "photonvision.local", 5800); // for photonvision
+
+    for (int port = 5800; port <= 5809; port++) 
+    {
+      PortForwarder.add(port+10, "limelight.local", port);
+    }
     // m_led.setLed(Constants.arduinoCOMs.Idle);
   }
   @Override

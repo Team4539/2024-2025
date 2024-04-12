ebf0360c8bf0a365bf2a98667aa773855a145d97
sstock2005 <sstock2005@outlook.com>
2024-04-12 14:04:00 -0500

    fix autoIntake

--- src/main/java/frc/robot/Commands/autoIntake.java
+++ src/main/java/frc/robot/Commands/autoIntake.java
@@ -2,7 +2,6 @@ package frc.robot.Commands;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -11,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.LimelightHelpers;
+import frc.robot.LimelightHelpers.LimelightResults;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
@@ -31,15 +31,12 @@ public class autoIntake extends Command
     private final double X_CLOSE_THRESHOLD = 6;
     private final double Y_CLOSE_THRESHOLD = 6;
 
-    private final PIDController pidController;
-
     public autoIntake(VisionSubsystem subsystem, CommandSwerveDrivetrain drive_subsystem, IntakeSubsystem intake) 
     {
         addRequirements(subsystem, drive_subsystem, intake);
         m_vision = subsystem;
         m_drive = drive_subsystem;
         m_intake = intake;
-        pidController = new PIDController(10, 0.0, 0); // TODO: tune this
     }
 
     @Override
@@ -55,6 +52,8 @@ public class autoIntake extends Command
         // left is down x right is up x towards robot is down y away from robot is up y
         double tx = LimelightHelpers.getTX("limelight-main");
         double ty = LimelightHelpers.getTY("limelight-main");
+        SmartDashboard.putNumber("tx", tx);
+        SmartDashboard.putNumber("ty", ty);
         if (tx != 0.0 && ty != 0.0)
         {
             double xCorrection = calculateCorrection(tx);
@@ -62,13 +61,16 @@ public class autoIntake extends Command
         
             applyCorrection(tx, ty, xCorrection, yCorrection);
         }
-
+        else
+        {
+            m_drive.setControl(forwardStraight.withRotationalRate(0));
+        }
     }
 
     @Override
     public void end(boolean interrupted) 
     {
-        m_drive.setControl(forwardStraight.withVelocityY(0.0));
+        m_drive.setControl(forwardStraight.withVelocityX(0).withVelocityY(0.0).withRotationalRate(0));
         m_intake.setIntake(0);
     }
 
@@ -86,30 +88,20 @@ public class autoIntake extends Command
         }
     }
     
-    private void applyCorrection(double tx, double ty, double xCorrection, double yCorrection) 
-    {
-        if (Math.abs(tx) > 0.05) 
-        {
-            double output = pidController.calculate(xCorrection, tx);
-            if (output < 1){ output = 1; }
-            SmartDashboard.putNumber("PID", output);
+    private void applyCorrection(double tx, double ty, double xCorrection, double yCorrection) {
+        if (Math.abs(tx) > 0.05) {
             m_drive.setControl(forwardStraight.withRotationalRate(xCorrection));
-        } 
-        else 
-        {
+        } else {
             x_centered = true;
         }
     
-        if (Math.abs(ty) > 0.05) 
-        {
+        if (Math.abs(ty) > 0.05) {
             m_drive.setControl(forwardStraight.withVelocityX(yCorrection > 0 ? 2 : 0));
-        } else 
-        {
+        } else {
             y_centered = true;
         }
     
-        if (Math.abs(tx) < X_CLOSE_THRESHOLD || Math.abs(ty) < Y_CLOSE_THRESHOLD) 
-        {
+        if (Math.abs(tx) < X_CLOSE_THRESHOLD || Math.abs(ty) < Y_CLOSE_THRESHOLD) {
             m_intake.setIntake(Constants.Intake.Speed);
         }
     

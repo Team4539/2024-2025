21731abdd883d1818aa2d14ecda21c2c5e57724e
Agame7k <comaidenaiden@gmail.com>
2024-10-11 15:00:49 -0500

    added more temps for diagnosics

--- src/main/java/frc/robot/subsystems/limitSwitchSubsystem.java
+++ src/main/java/frc/robot/subsystems/limitSwitchSubsystem.java
@@ -11,6 +11,17 @@ public class limitSwitchSubsystem extends SubsystemBase
 {
     private DigitalInput limitswitch;
     private final TalonFX Motor1 = new TalonFX(1);
+    private final TalonFX Motor2 = new TalonFX(2);
+    private final TalonFX Motor3 = new TalonFX(3);
+    private final TalonFX Motor4 = new TalonFX(4);
+    private final TalonFX Motor5 = new TalonFX(5);
+    private final TalonFX Motor6 = new TalonFX(6); 
+    private final TalonFX Motor7 = new TalonFX(7);
+    private final TalonFX Motor8 = new TalonFX(8);
+    private final TalonFX Motor14 = new TalonFX(14);
+    private final TalonFX Motor15 = new TalonFX(15);
+    private final TalonFX Motor16 = new TalonFX(16);
+
 
     
     public limitSwitchSubsystem()
@@ -21,7 +32,17 @@ public class limitSwitchSubsystem extends SubsystemBase
     public void periodic()
     {
         SmartDashboard.putBoolean("Limit Switch", getSwitch());
-        SmartDashboard.putString("Motor temp", ((Motor1.getDeviceTemp().getValue() * 9/5) + 32) + "\u00B0F");
+        SmartDashboard.putString("Module 0 Drive", ((Motor1.getDeviceTemp().getValue() * 9/5) + 32) + "\u00B0F");
+        SmartDashboard.putString("Module 0 Turn", ((Motor2.getDeviceTemp().getValue() * 9/5) + 32) + "\u00B0F");
+        SmartDashboard.putString("Module 1 Drive", ((Motor3.getDeviceTemp().getValue() * 9/5) + 32) + "\u00B0F");
+        SmartDashboard.putString("Module 1 Turn", ((Motor4.getDeviceTemp().getValue() * 9/5) + 32) + "\u00B0F");
+        SmartDashboard.putString("Module 2 Drive", ((Motor6.getDeviceTemp().getValue() * 9/5) + 32) + "\u00B0F");
+        SmartDashboard.putString("Module 2 Turn", ((Motor5.getDeviceTemp().getValue() * 9/5) + 32) + "\u00B0F");
+        SmartDashboard.putString("Module 3 Drive", ((Motor7.getDeviceTemp().getValue() * 9/5) + 32) + "\u00B0F");
+        SmartDashboard.putString("Module 3 Turn", ((Motor8.getDeviceTemp().getValue() * 9/5) + 32) + "\u00B0F");
+        SmartDashboard.putString("Intake Motor", ((Motor14.getDeviceTemp().getValue() * 9/5) + 32) + "\u00B0F");
+        SmartDashboard.putString("Shooter Motor 1", ((Motor15.getDeviceTemp().getValue() * 9/5) + 32) + "\u00B0F");
+        SmartDashboard.putString("Shooter Motor 2", ((Motor16.getDeviceTemp().getValue() * 9/5) + 32) + "\u00B0F");
 
     }
 
